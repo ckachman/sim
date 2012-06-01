59f97b3c6cbf4ea094e52af916e0913c4ce13988
unknown <z082417@9WGY4R1.hq.target.com>
2012-06-01 14:16:52 -0500

    getting everything working from end-to-ed with backstock

--- grails-app/controllers/tgt/BackstockController.groovy
+++ grails-app/controllers/tgt/BackstockController.groovy
@@ -15,29 +15,68 @@ class BackstockController {
 		//def item = new Item(sku:sku, description:"Men's Awesome Plaid Pants")
 		
 		//TODO - replace this with a real item locations lookup based on item sku
-		def itemLocations = []
+//		def itemLocations = []
+//		
+//		itemLocations << new ItemLocation(location: new Location(state: 'Open Stock', locId: 'ABC XYZ 123'), quantity: 3)
+//		itemLocations << new ItemLocation(location: new Location(state: 'Boxed', locId: 'XYZ ABC 321'), quantity: 4)
+//		itemLocations << new ItemLocation(location: new Location(state: 'Open Stock', locId: 'XYA CBZ 456'), quantity: 5)
 		
-		itemLocations << new ItemLocation(location: new Location(state: 'Open Stock', locId: 'ABC XYZ 123'), quantity: 3)
-		itemLocations << new ItemLocation(location: new Location(state: 'Boxed', locId: 'XYZ ABC 321'), quantity: 4)
-		itemLocations << new ItemLocation(location: new Location(state: 'Open Stock', locId: 'XYA CBZ 456'), quantity: 5)
+		session.item = item
 		
-		[itemLocations: itemLocations, item: item]		
+		[itemLocations: item.itemLocations, item: item]		
 	}
 	
 	// edit item location
 	def editItemLocation = {
 		println "params: $params"
-		def itemloc = new ItemLocation(location: new Location(locId: params.txtLocationScan), quantity:3, 
-				item: new Item(sku:'123123', description:'awesome item', state: 'itemstate'))
+		
+		def locId = params.txtLocationScan
+		def item = session.item
+		def location = Location.findByLocId(locId)
+		
+		def itemloc = new ItemLocation(location:location, quantity:0, item:item)
+		
+		println "loc id: $locId"
+		println "item: $item"
+		println "location: $location"
+		println "item loc: $itemloc"
+		
+		session.location = location
+		
 		[itemloc: itemloc]
 	}
 	
 	def updateItemLocation = {
 		println "params: $params"
 		
-		def quantity = params.txtItemQty
+		def quantity = params.txtItemQty ? params.int('txtItemQty') : 0
+		def location = session.location
+		def item = session.item
+		
+		println "quantity: $quantity"
+		println "location: $location"
+		println "item: $item"
+		
+		def itemLocation = ItemLocation.findByLocationAndItem(location, item)
+		
+		if(!itemLocation){
+			println "item location does not exist - creating a new one"
+			itemLocation = new ItemLocation(location:location, item:item)
+		}
+		
+		itemLocation.quantity += quantity
+		
 		
 		// update itemlocation quantity
+		if(itemLocation.save()){
+			println "successfully updated item location quantity to $itemLocation.quantity"
+		}else{
+			println "failed to update item location: $itemLocation.errors"
+		}
+		
+		session.location = null
+		session.item = null
+		
 		
 		[]
 
