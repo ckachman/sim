fb6aaa8d6c641773e215af3e7950910376f91fd3
unknown <z082417@9WGY4R1.hq.target.com>
2012-06-01 12:21:41 -0500

    getting the backstock itemLocations action working

--- grails-app/controllers/tgt/BackstockController.groovy
+++ grails-app/controllers/tgt/BackstockController.groovy
@@ -4,40 +4,44 @@ class BackstockController {
 	
 	def stub = """
 {
-	"status":				"success",
-	"page":					"BCK001",		
-	
+	"status":		"success",
+	"page":			"BCK002",
 	"config": {
 		"beepCount":		2,
+		"appLabel":			"Backstock",
+		"leftURL":			"ajax/ajax_Home.chtm",
 		"homeURL":			"ajax/ajax_Home.chtm",
 		"toggleURL":		"ajax/ajax_Home.chtm",
 		"showKeyboard":		true,
-		"fieldLabel":		"<b>Scan</b>/<b>Key</b> item barcode",
-		"fieldID":			"txtItemScan",
-		"formID":			"formBCK001",
-		"formAction":		"ajax/ajax_BCK001.chtm",
-		"appLabel":			"Backstock",
 		"line_1":			"",
-		"line_2":			"",
-		"line_3":			"",
-		"line_3_right":		""
+		"line_2":			"490581002089",
+		"line_3":			"Men's Denim Shorts",
+		"fill_group":		"Fill group: Mens",
+		"fieldLabel":		"<b>Scan</b>/<b>Key</b> location",
+		"fieldID":			"txtLocationScan",
+		"formAction":		"ajax/ajax_BCK002.chtm",
+		"formID":			"formBCK002"
 	},
 		
-	
-	"strings": {
-		"appName":			"Backstock",
-		"page":				"Page",
-		"of":				"of",
-		"close":			"Close",
-		"enter":			"Enter"
-	}
-	
+	"gridTable": {
+		"tableTitle":		"",
+		"data":	[
+			{"Location":"01A 001G02","Group":"Open Stock","Qty":"3"},
+			{"Location":"01A 001G03","Group":"Open Stock","Qty":"13"},
+			{"Location":"01A 001G04","Group":"Open Stock","Qty":"10"},
+			{"Location":"01A 001G05","Group":"Open Stock","Qty":"2"},
+			{"Location":"01A 001G06","Group":"Open Stock","Qty":"4"}
+		]
+	}	
 }"""
 
 
 	// uri /backstock/itemLocations/${itemSku}
     def itemLocations = {
+		log.debug "params: $params"
+		
 		//find item locations in the back room
+		response.contentType = "application/json"
 		render stub
 	}
 
