877ac9c4b2dbbab10c7be171b08229384fba3104
unknown <z082417@9WGY4R1.hq.target.com>
2012-06-01 12:49:09 -0500

    more fun backstock controller work

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -79,6 +79,11 @@ log4j = {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
 
+	debug  'grails.app.bootstrap',
+		'org.codehaus.groovy.grails.web.servlet',
+		'grails.app.service',
+		'grails.app.filters'
+	
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
            'org.codehaus.groovy.grails.web.pages', //  GSP
            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
--- grails-app/controllers/tgt/BackstockController.groovy
+++ grails-app/controllers/tgt/BackstockController.groovy
@@ -4,7 +4,11 @@ class BackstockController {
 
 	// uri /backstock/itemLocations/${itemSku}
     def itemLocations = {
-		log.debug "params: $params"
+		println "params: $params"
+		
+		
+		def sku = params.txtItemScan
+		def item = new Item(sku:sku, description:"Men's Awesome Plaid Pants")
 		
 		//TODO - replace this with a real item locations lookup based on item sku
 		def itemLocations = []
@@ -13,7 +17,7 @@ class BackstockController {
 		itemLocations << new ItemLocation(location: new Location(state: 'Boxed', locId: 'XYZ ABC 321'), quantity: 4)
 		itemLocations << new ItemLocation(location: new Location(state: 'Open Stock', locId: 'XYA CBZ 456'), quantity: 5)
 		
-		[itemLocations: itemLocations]		
+		[itemLocations: itemLocations, item: item]		
 	}
 
 }
